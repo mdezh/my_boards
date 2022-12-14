994287f8635f8b6d62c5362974b41400fe99e9b4
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2022-12-15 00:23:25 +0300

    add rooms seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do
+  name = Faker::Hobby.activity
+  description = Faker::Lorem.sentence(word_count: 10)
+  Room.create!(name:, description:)
+end
