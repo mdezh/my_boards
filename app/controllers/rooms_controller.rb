b40c46c3bb9356a04322b5aef0221efe950d9012
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2022-12-15 01:19:31 +0300

    add edit/update room functionality

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,26 @@
 class RoomsController < ApplicationController
+  before_action :set_room, only: %i[edit update]
+
   def index
     @rooms = Room.all
+  end
+
+  def update
+    if @room.update(room_params)
+      flash[:success] = 'Room changed'
+      redirect_to root_path
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(:name, :description)
   end
 end
