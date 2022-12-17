9ea8d25a2cf58517c7aa14a1dc9caacc54c111f8
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2022-12-17 08:11:35 +0300

    improve seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 10.times do
-  name = (1..5).map { Faker::Hobby.activity }.join(' ')
+  name = (1..rand(1..5)).map { Faker::Hobby.activity }.join(' ')
   description = Faker::Lorem.sentence(word_count: 1, supplemental: true, random_words_to_add: 30)
   Room.create!(name:, description:)
 end
