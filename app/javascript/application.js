099472587c8fbfa53da914bcd68dc66401ad235f
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2022-12-17 03:56:42 +0300

    add popovers initialization with ability to insert html

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -14,3 +14,22 @@ import "bootstrap";
 //     (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 //   );
 // });
+
+// Initialize popovers on the page
+// Add html content for popovers with .popover-with-html
+document.addEventListener("turbo:load", () => {
+  const popoverTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="popover"]'
+  );
+  [...popoverTriggerList].forEach((element) => {
+    if (element.classList.contains("popover-with-html")) {
+      const id = element.id;
+      const elemForContent = document.querySelector(`#${id}-html`);
+      if (elemForContent) {
+        element.setAttribute("data-bs-html", "true");
+        element.setAttribute("data-bs-content", elemForContent.innerHTML);
+      }
+    }
+    new bootstrap.Popover(element);
+  });
+});
