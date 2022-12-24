5ed95660b82d4b4256f789e5d33c444895b40016
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2022-12-25 01:12:21 +0300

    add validation to room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,5 @@
 class Room < ApplicationRecord
   has_many :posts, dependent: :destroy
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :description, length: { maximum: 1000 }
 end
