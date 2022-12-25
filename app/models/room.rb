ce530886cadcae1a26dfca3ee5abb246b68c4557
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2022-12-26 01:37:48 +0300

    add uniqueness validation for room names

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
   has_many :posts, dependent: :destroy
-  validates :name, presence: true, length: { maximum: 100 }
+  validates :name, presence: true, uniqueness: true, length: { maximum: 100 }
   validates :description, length: { maximum: 1000 }
 end
