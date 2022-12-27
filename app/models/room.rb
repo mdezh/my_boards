f7035651ef7582d311c3fb1560a554a8eac6e508
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2022-12-28 00:02:45 +0300

    strip room descriptions too

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,12 +3,13 @@ class Room < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { maximum: 100 }
   validates :description, length: { maximum: 1000 }
 
-  before_validation :prepare_name
+  before_validation :prepare_fields
 
   private
 
-  def prepare_name
+  def prepare_fields
     # with name.strip! form field after invalid submit will stay unstripped
     self.name = name.strip
+    self.description = description.strip
   end
 end
