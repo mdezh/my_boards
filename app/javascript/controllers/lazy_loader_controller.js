827bbc7ab8398ff1cd8d1eb7fd9c4dec8269092e
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2022-12-28 23:35:19 +0300

    add loading indicator for rooms infinite scrolling

--- app/javascript/controllers/lazy_loader_controller.js
+++ app/javascript/controllers/lazy_loader_controller.js
@@ -5,35 +5,47 @@ export default class extends Controller {
   static values = {
     src: String,
     status: String,
+    indicator: String,
   };
 
   connect() {
     this.observer = new IntersectionObserver((entries) => {
       const lastEntry = entries.slice(-1)[0];
-      if (lastEntry?.isIntersecting) {
-        this.load();
-      }
+      lastEntry?.isIntersecting && this.load();
     });
+    this.indicator = this.indicatorValue
+      ? document.querySelector(this.indicatorValue)
+      : null;
     this.observer.observe(this.element);
   }
 
   load() {
     if (!this.statusValue) {
       this.statusValue = "pending";
+      this.showIndicator();
       fetch(this.srcValue, {
         headers: {
           Accept: "text/vnd.turbo-stream.html",
         },
       })
         .then((r) => r.text())
-        .then((html) => Turbo.renderStreamMessage(html))
+        .then((html) => (this.hideIndicator(), Turbo.renderStreamMessage(html)))
         .then(() => (this.statusValue = "complete"))
         .then(() => this.observer.unobserve(this.element))
         .catch((e) => {
+          this.hideIndicator();
           console.log("Something goes wrong on lazy loading:", e);
           this.statusValue = "";
         });
     }
+  }
+
+  showIndicator() {
+    this.indicator?.classList.remove("invisible");
+  }
+
+  hideIndicator() {
+    this.indicator?.classList.add("invisible");
   }
 
   disconnect() {
