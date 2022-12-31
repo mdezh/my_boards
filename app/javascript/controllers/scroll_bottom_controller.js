d265b6b1e4d3e1a0912df47ab67cd34bcc706397
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2022-12-31 22:22:04 +0300

    improve scrolling of notes to bottom

--- app/javascript/controllers/scroll_bottom_controller.js
+++ app/javascript/controllers/scroll_bottom_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="scroll-bottom"
 export default class extends Controller {
   connect() {
-    this.element.scrollTop = this.element.scrollHeight;
+    this.element.scrollTop =
+      this.element.scrollHeight - this.element.clientHeight;
   }
 }
