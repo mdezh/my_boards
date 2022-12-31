6fa9e5ff29b8922671ae96969845cb606557d4bc
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2022-12-31 20:03:33 +0300

    improve links to board in address line (now from root)

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,8 @@
 class MainController < ApplicationController
   def show
     @active_board_id = params[:board]
+    return if request.headers.to_h['HTTP_TURBO_FRAME'].nil?
+
+    NotesController.dispatch(:index, request, response)
   end
 end
