f2e21ee349ccacdc913ce34dad28c56b5aeb46ed
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-01-02 01:23:50 +0300

    strip note content before save

--- app/models/board.rb
+++ app/models/board.rb
@@ -3,11 +3,11 @@ class Board < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { maximum: 100 }
   validates :description, length: { maximum: 1000 }
 
-  before_validation :prepare_fields
+  before_validation :strip_fields
 
   private
 
-  def prepare_fields
+  def strip_fields
     # with name.strip! form field after invalid submit will stay unstripped
     self.name = name.strip
     self.description = description.strip
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,11 @@
 class Note < ApplicationRecord
   belongs_to :board
+
+  before_validation :strip_content
+
+  private
+
+  def strip_content
+    content.strip!
+  end
 end
