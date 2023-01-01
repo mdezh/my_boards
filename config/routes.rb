59cb0ee3dc141b189b6d1b300d6828d3182f7142
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-01-02 00:19:32 +0300

    add 'only' option for notes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'main#show'
   resources :boards do
-    resources :notes, shallow: true
+    resources :notes, shallow: true, only: %i[index create]
     member do
       get :details
     end
