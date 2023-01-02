4d53effbff25e60aa55d7f5614bec232f6b5c006
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-01-02 17:44:24 +0300

    fix bug with scrolling prev element when change growing to shrinking

--- app/javascript/controllers/auto_grow_controller.js
+++ app/javascript/controllers/auto_grow_controller.js
@@ -11,14 +11,20 @@ export default class extends Controller {
   }
 
   grow() {
-    const oldHeight = this.element.clientHeight;
-    this.element.style.height = this.element.scrollHeight + 2 + "px";
     const prevElement = this.prevValue
       ? document.querySelector(this.prevValue)
       : null;
+    let oldHeight, oldTop;
     if (prevElement) {
-      const deltaHeight = this.element.clientHeight - oldHeight;
-      prevElement.scrollTop += deltaHeight;
+      oldHeight = prevElement.clientHeight;
+      oldTop = prevElement.scrollTop;
+    }
+    // next line is necessary for shrinking
+    this.element.style.height = "";
+    this.element.style.height = this.element.scrollHeight + 2 + "px";
+    if (prevElement) {
+      const deltaHeight = prevElement.clientHeight - oldHeight;
+      prevElement.scrollTop = oldTop - deltaHeight;
     }
   }
 }
