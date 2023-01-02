e4ee373de64500daff0470c4a0c70647735cc790
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-01-02 17:22:57 +0300

    add posibility to scroll other element to save visible position when textarea grows

--- app/javascript/controllers/auto_grow_controller.js
+++ app/javascript/controllers/auto_grow_controller.js
@@ -2,11 +2,23 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="auto-grow"
 export default class extends Controller {
+  static values = {
+    prev: String,
+  };
+
   connect() {
     this.grow();
   }
 
   grow() {
+    const oldHeight = this.element.clientHeight;
     this.element.style.height = this.element.scrollHeight + 2 + "px";
+    const prevElement = this.prevValue
+      ? document.querySelector(this.prevValue)
+      : null;
+    if (prevElement) {
+      const deltaHeight = this.element.clientHeight - oldHeight;
+      prevElement.scrollTop += deltaHeight;
+    }
   }
 }
