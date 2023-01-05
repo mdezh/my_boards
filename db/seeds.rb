c9129bf436b7eae45306f8474466ef1d89f282c7
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-01-05 13:14:24 +0300

    improve seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 100.times do
-  name = (1..rand(1..5)).map { Faker::Hobby.activity }.join(' ')
-  description = Faker::Lorem.sentence(word_count: 1, supplemental: true, random_words_to_add: 30)
+  name = Faker::Hobby.unique.activity
+  description = Faker::Books::Lovecraft.sentence(word_count: 1, random_words_to_add: 30)
   Board.create(name:, description:)
 end
 
