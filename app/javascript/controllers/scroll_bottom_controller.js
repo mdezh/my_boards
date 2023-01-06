92860e3752544828b00f1eb4c398c71d8fde8650
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-01-07 02:09:57 +0300

    remove unnecessary scroll_bottom controller

--- app/javascript/controllers/scroll_bottom_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-
-// Connects to data-controller="scroll-bottom"
-export default class extends Controller {
-  connect() {
-    this.element.scrollTop =
-      this.element.scrollHeight - this.element.clientHeight;
-  }
-}
