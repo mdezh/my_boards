ae51ff7081fd6dc2454b1ba0dfdc45f5213cbc22
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-01-07 22:34:48 +0300

    introduce X-UTC-Offset header in requests and @utc_offset variable in app. controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_utc_offset
+
+  private
+
+  def set_utc_offset
+    @utc_offset = request.headers['X-UTC-Offset']&.to_i || 0
+  end
 end
