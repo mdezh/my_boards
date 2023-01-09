13068a882a498ee5f2f9cb65d7dd95fd5efc71b3
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-01-09 03:44:39 +0300

    use time zone from the client request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  around_action :set_time_zone
+
+  private
+
+  def set_time_zone(&block)
+    time_zone = request.headers['X-Time-Zone'] || 'UTC'
+    Time.use_zone(time_zone, &block)
+  end
 end
