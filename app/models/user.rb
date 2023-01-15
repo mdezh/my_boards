f5a5b8c3373c74fc4c821e4d2c63296684f89d6b
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-01-15 23:41:02 +0300

    devise: remove email, use nickname for authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
+  # Include default devise modules. Others available are: :recoverable,
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
+
+  validates_presence_of :nickname
+  validates_uniqueness_of :nickname
+
+  # next two lines prevent email validation by devise (as we removed this field)
+  def email_required? = false
+  def will_save_change_to_email? = false
 end
