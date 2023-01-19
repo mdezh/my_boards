a8347c955ee151d1623cdf08d7947487bd2c67cc
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-01-19 22:22:40 +0300

    change name for notes turbo stream channel

--- app/models/note.rb
+++ app/models/note.rb
@@ -4,7 +4,7 @@ class Note < ApplicationRecord
 
   before_validation :strip_content
 
-  broadcasts_to ->(note) { ActionView::RecordIdentifier.dom_id(note.board) }, inserts_by: :prepend
+  broadcasts_to ->(note) { [note.board, :notes] }, inserts_by: :prepend
 
   private
 
