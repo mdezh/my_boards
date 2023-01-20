d8333bb8ffb650490639c5b7e328061a59cd6c2e
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-01-20 03:29:02 +0300

    handle enter and esc keys in textarea in edit note form

--- app/javascript/controllers/clicker_controller.js
+++ app/javascript/controllers/clicker_controller.js
@@ -3,6 +3,8 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="click-elements"
 export default class extends Controller {
   static values = {
+    submit: String,
+    cancel: String,
     selector: String,
     prevent: { type: Boolean, default: false },
   };
@@ -11,5 +13,15 @@ export default class extends Controller {
     this.preventValue && e.preventDefault();
     const elements = document.querySelectorAll(this.selectorValue);
     elements && elements.forEach((element) => element.click());
+  }
+
+  submit(e) {
+    e.preventDefault();
+    document.querySelector("#" + this.submitValue)?.click();
+  }
+
+  cancel(e) {
+    e.preventDefault();
+    document.querySelector("#" + this.cancelValue)?.click();
   }
 }
