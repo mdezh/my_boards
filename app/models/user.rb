e01e4de98e33444f926dc9872b5cfece8a6748a4
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-01-21 23:57:41 +0300

    add extra validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :boards, through: :relations
 
   validates_presence_of :nickname
+  validates_length_of :nickname, within: 1..30
+  validates_format_of :nickname, with: /\A[a-z][a-z0-9_]*\z/i
   validates_uniqueness_of :nickname
 
   # next two lines prevent email validation by devise (as we removed this field)
