c1768e897e7aadd2385b39dee2d07d80850ef802
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-01-23 03:37:54 +0300

    destroy boards with belonging entities when owner is destroyed

--- app/models/relation.rb
+++ app/models/relation.rb
@@ -6,14 +6,25 @@ class Relation < ApplicationRecord
   validates :board_id, uniqueness: { scope: :user_id }
   validate :board_name_should_be_unique_per_user, on: :create
 
+  after_destroy ->(record) { destroy_board_if_owner record }
+
   enum role: %i[owner subscriber]
 
   private
 
   def board_name_should_be_unique_per_user
-    board_name = board.name
-    return unless user.boards.where('name = ?', board_name).count > 0
+    return unless user.boards.where('name = ?', board.name).count.positive?
 
     errors.add(:name, 'should be unique per user')
+  end
+
+  def destroy_board_if_owner(record)
+    return unless record.owner?
+
+    # use this manual deleting instead of record.board.destroy for performance
+    # TODO: put this into an async job
+    Note.where(board: record.board).delete_all # without broadcasting, as we will destroy the board anyway
+    Relation.where(board: record.board).delete_all
+    record.board.destroy  # use destroy instead of delete here as we need broadcasting
   end
 end
