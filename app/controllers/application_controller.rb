82190e9c86580e68c47e1306634575ce8e3f4967
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-01-25 10:11:29 +0300

    set Current.user in Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,15 @@ class ApplicationController < ActionController::Base
   # looks like it works without next line
   # protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :set_current_user
   around_action :set_time_zone
 
   private
+
+  def set_current_user
+    Current.skip_user_check = false
+    Current.user = current_user
+  end
 
   def set_time_zone(&block)
     time_zone = request.headers['X-Time-Zone'] || 'UTC'
