4577a1ab016d73b95d97cef25e19908786d337d4
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-02-01 19:14:23 +0300

    use state-driven stimulus controller instead of specialized ones in notes header dropdown

--- app/javascript/controllers/state_driven_controller.js
+++ app/javascript/controllers/state_driven_controller.js
@@ -1,9 +1,14 @@
 import { UseStateController } from "../base_classes/use_state_controller";
 
-// Connects to data-controller="notes-header-dropdown"
+// Connects to data-controller="state-driven"
 export default class extends UseStateController {
+  static values = {
+    ...super.values,
+    key: String,
+  };
+
   updateWithState(state) {
-    if (state.owned || state.joined) {
+    if (state[this.keyValue]) {
       this.element.classList.remove(this.hiddenClassValue);
     } else {
       this.element.classList.add(this.hiddenClassValue);
