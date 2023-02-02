5a8f83da048974105c89ce26fbafbda8220a162e
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-02-02 13:08:01 +0300

    decrease max length for nickname to 20

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :notes, dependent: :destroy
 
   validates_presence_of :nickname
-  validates_length_of :nickname, within: 1..30
+  validates_length_of :nickname, within: 1..20
   validates_format_of :nickname, with: /\A[a-z][a-z0-9_]*\z/i
   validates_uniqueness_of :nickname
 
