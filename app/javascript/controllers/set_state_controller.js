5e23b8f7d2da677a2fda442c30305fdaab70ab6a
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-02-03 23:38:25 +0300

    add onConnect boolean value for set-state controller, fix dispatchEvent

--- app/javascript/controllers/set_state_controller.js
+++ app/javascript/controllers/set_state_controller.js
@@ -5,10 +5,17 @@ export default class extends Controller {
   static values = {
     object: Object,
     id: String,
+    onConnect: { type: Boolean, default: false },
   };
 
   connect() {
-    window.dispatch(
+    if (this.onConnectValue) {
+      this.fire();
+    }
+  }
+
+  fire() {
+    window.dispatchEvent(
       new CustomEvent(`set_${this.idValue}`, {
         detail: { ...this.objectValue },
       })
