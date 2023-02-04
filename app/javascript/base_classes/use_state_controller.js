2c7702e986710dc1b01f419aa8f1613b8ebc7084
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-02-04 07:35:35 +0300

    allow using selector instead of state outlet in UseStateController

--- app/javascript/base_classes/use_state_controller.js
+++ app/javascript/base_classes/use_state_controller.js
@@ -1,32 +1,70 @@
 import { Controller } from "@hotwired/stimulus";
 
+const camelize = (s) => s.replace(/-./g, (x) => x[1].toUpperCase());
+
 export default class UseStateController extends Controller {
+  // use outlets or stateSelectorValue (there are issues with outlets sometimes,
+  // also selector is useful for state controller children classes)
   static outlets = ["state"];
   static values = {
     hiddenClass: { type: String, default: "hidden" },
     primarySelector: { type: String, default: "" },
     secondarySelector: { type: String, default: "" },
+    stateSelector: { type: String, default: "" },
   };
 
   connect() {
-    const actions = this.stateOutlets.map(
-      (outlet) => `${outlet.element.id}@window->${this.identifier}#refresh`
-    );
-    this.element.dataset.action = [
-      ...(this.element.dataset.action ?? "").split(" ").filter(Boolean),
-      ...actions,
-    ].join(" ");
-
-    const states = this.stateOutlets.map((outlet) => outlet.objectValue);
-    this.state = states.reduce((acc, state) => Object.assign(acc, state), {});
-
     this.primaryElements = this.primarySelectorValue
-      ? querySelectorAll(this.primarySelectorValue)
+      ? document.querySelectorAll(this.primarySelectorValue)
       : [this.element];
 
     this.secondaryElements = this.secondarySelectorValue
-      ? querySelectorAll(this.secondarySelectorValue)
+      ? document.querySelectorAll(this.secondarySelectorValue)
       : undefined;
+
+    this.initActionsAndState();
+  }
+
+  getStateSources() {
+    let sources;
+    if (this.hasStateOutlet) {
+      sources = this.stateOutlets;
+    } else if (this.stateSelectorValue) {
+      const elements = document.querySelectorAll(this.stateSelectorValue);
+      if (elements.length > 0) {
+        // dirty hack with duck typing
+        sources = [...elements].map((element) => {
+          const controllerName = element.dataset.controller
+            .split(" ")
+            .find((name) => name.startsWith("state"));
+          if (!controllerName) throw Error("Failed to find state controller");
+          const dataPropName = camelize(controllerName) + "ObjectValue";
+          return {
+            element,
+            objectValue: JSON.parse(element.dataset[dataPropName]),
+          };
+        });
+      }
+    }
+    if (!sources) {
+      throw Error("Data sources are not defined or don't exist!");
+    }
+    return sources;
+  }
+
+  initActionsAndState() {
+    const sources = this.getStateSources();
+
+    const actions = sources.map(
+      (outlet) => `${outlet.element.id}@window->${this.identifier}#refresh`
+    );
+
+    this.element.dataset.action = [this.element.dataset.action, ...actions]
+      .join(" ")
+      .trim();
+
+    const states = sources.map((outlet) => outlet.objectValue);
+    this.state = states.reduce((acc, state) => Object.assign(acc, state), {});
 
     this.updateWithState();
   }
@@ -59,5 +97,3 @@ export default class UseStateController extends Controller {
     toHide?.forEach((element) => element.classList.add(this.hiddenClassValue));
   }
 }
-
-export { UseStateController };
