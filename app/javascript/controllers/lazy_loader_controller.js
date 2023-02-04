04d685516e2d66229becb7c0563359ca213ffe85
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-02-04 07:37:02 +0300

    use console.error instead of .log in lazy_loader

--- app/javascript/controllers/lazy_loader_controller.js
+++ app/javascript/controllers/lazy_loader_controller.js
@@ -37,7 +37,7 @@ export default class extends Controller {
         .then(() => this.observer.unobserve(this.element))
         .catch((e) => {
           this.hideIndicator();
-          console.log("Something goes wrong on lazy loading:", e);
+          console.error("Something goes wrong on lazy loading:", e);
           this.statusValue = "";
         });
     }
