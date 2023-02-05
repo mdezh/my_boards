f4836d319d8c826f31d115dfe074b2a1f32b0921
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-02-06 02:01:38 +0300

    improve state-driven controller: now it can work with list of keys with possible ! mod at each of them

--- app/javascript/controllers/state_driven_controller.js
+++ app/javascript/controllers/state_driven_controller.js
@@ -6,12 +6,35 @@ export default class extends UseStateController {
     ...super.values,
     key: { type: String, default: "counter" }, // for state-counter controller
     not: { type: Boolean, default: false },
-    eql: { type: String, default: "" },
+    eql: String,
+    keys: Array, // array of state keys for checkFunction, possible with !: ["a", "b", "!c"]
+    and: { type: Boolean, default: false }, // use && instead of default || to combine state values
   };
 
+  connect() {
+    this.combinedFunction = this.buildFunction();
+    super.connect();
+  }
+
+  buildFunction() {
+    const keys = this.hasKeysValue ? this.keysValue : [this.keyValue];
+    const operands = keys.map((key) => {
+      if (key.startsWith("!")) {
+        key = key.slice(1);
+        return (state) => !state[key];
+      }
+      return (state) => state[key];
+    });
+    const reducedFunction = this.andValue
+      ? (state) => operands.reduce((acc, f) => acc && f(state), true)
+      : (state) => operands.reduce((acc, f) => acc || f(state), false);
+    const expectation = this.hasEqlValue ? this.eqlValue : true;
+    return this.notValue
+      ? (state) => reducedFunction(state) != expectation
+      : (state) => reducedFunction(state) == expectation;
+  }
+
   checkFunction() {
-    const result =
-      this.state[this.keyValue] == (this.eqlValue ? this.eqlValue : true);
-    return this.notValue ? !result : result;
+    return this.combinedFunction(this.state);
   }
 }
