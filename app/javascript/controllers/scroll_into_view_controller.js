0c2c81c31386bc5d4fe3f7518677b5950eacb1b6
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-02-07 00:45:06 +0300

    improve scroll-into-view controller: add options value

--- app/javascript/controllers/scroll_into_view_controller.js
+++ app/javascript/controllers/scroll_into_view_controller.js
@@ -2,7 +2,15 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="scroll-into-view"
 export default class extends Controller {
+  static values = {
+    options: Object,
+  };
+
   connect() {
-    this.element.scrollIntoView();
+    if (this.hasOptionsValue) {
+      this.element.scrollIntoView(this.optionsValue);
+    } else {
+      this.element.scrollIntoView();
+    }
   }
 }
