76893c4b80befca2c7e2ba80ad1c9e7e7ffee197
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-02-07 22:28:35 +0300

    add helpers for request format checking

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def turbo_frame?
+    request.headers['HTTP_TURBO_FRAME'].present?
+  end
+
+  def turbo_stream?
+    formats.include?(:turbo_stream)
+  end
+
+  def turbo?
+    turbo_frame? || turbo_stream?
+  end
 end
