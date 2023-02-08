5e728be844c2bcbead9013f8f1e6b3a9cc5b376a
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-02-08 19:50:47 +0300

    improve state controller

--- app/javascript/controllers/state_controller.js
+++ app/javascript/controllers/state_controller.js
@@ -1,49 +1,65 @@
 import { Controller } from "@hotwired/stimulus";
+import { parse, fire } from "helpers";
 
 // Connects to data-controller="state"
 export default class StateController extends Controller {
   static values = {
+    init: { type: String, default: "false" },
     object: { type: Object, default: {} },
   };
 
   connect() {
-    this.addActions();
+    this.objectValue = {
+      state: parse(this.initValue),
+      trigger: 0,
+    };
+    this._addActions();
   }
 
-  addActions() {
+  fireState(e) {
+    let eventName = this.element.id;
+    const receiver = e.detail;
+    if (typeof receiver == "string") {
+      eventName += `_to_${receiver}`;
+    }
+    this._fireStateWithName(eventName);
+  }
+
+  setState(e) {
+    this._updateState(e.detail);
+  }
+
+  objectValueChanged() {
+    this._fireStateWithName(this.element.id);
+  }
+
+  _updateState(changes) {
+    let state = this.objectValue.state;
+    if (typeof state != typeof changes) {
+      throw Error("The update doesn't match the state type");
+    }
+
+    state = typeof state == "object" ? { ...state, ...changes } : changes;
+
+    // use a trigger to ensure the event fires even if the state remains the same
+    const trigger = 1 + (this.objectValue.trigger ?? 0);
+    this.objectValue = {
+      state,
+      trigger,
+    };
+  }
+
+  _addActions() {
     this.element.dataset.action = [
       this.element.dataset.action,
-      `fire_${this.element.id}@window->state#fireState`,
-      `set_${this.element.id}@window->state#setState`,
+      `fire_${this.element.id}@window->${this.identifier}#fireState`,
+      `set_${this.element.id}@window->${this.identifier}#setState`,
     ]
       .join(" ")
       .trim();
   }
 
-  fireState() {
-    const value = { ...this.objectValue };
-    delete value._trigger;
-    const event = new CustomEvent(this.element.id, {
-      detail: value,
-    });
-    window.dispatchEvent(event);
-  }
-
-  setState(e) {
-    this.updateState(e.detail);
-  }
-
-  updateState(changes) {
-    // use _trigger to guarantee event firing even when the state remains the same
-    let _trigger = 1 + (this.objectValue._trigger ?? 0);
-    this.objectValue = {
-      ...this.objectValue,
-      ...changes,
-      _trigger,
-    };
-  }
-
-  objectValueChanged() {
-    this.fireState();
+  _fireStateWithName(name) {
+    fire(name, this.objectValue.state);
   }
 }
