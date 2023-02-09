a3e591607162a90154b7b4c069bb5f61b131bc80
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-02-09 09:21:46 +0300

    remove add-note controller, use show instead

--- app/javascript/controllers/add_note_controller.js
+++
@@ -1,8 +0,0 @@
-import UseStateToToggleController from "controllers/use_state_to_toggle_controller";
-
-// Connects to data-controller="add-note"
-export default class extends UseStateToToggleController {
-  checkFunction() {
-    return this.state.owned || (this.state.joined && this.state.public_rw);
-  }
-}
