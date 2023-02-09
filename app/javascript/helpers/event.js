956d2190afa1eebdbc26ba976e876ea97ef98f2d
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-02-09 09:16:08 +0300

    fire() helper returns result of dipatchEvent()

--- app/javascript/helpers/event.js
+++ app/javascript/helpers/event.js
@@ -1,3 +1,3 @@
 export function fire(name, detail) {
-  window.dispatchEvent(new CustomEvent(name, detail ? { detail } : {}));
+  return window.dispatchEvent(new CustomEvent(name, detail ? { detail } : {}));
 }
