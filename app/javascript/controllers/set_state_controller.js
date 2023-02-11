4ed217b5c0259522da9108fb65d06d5e9fedf132
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-02-11 06:56:24 +0300

    prevent repeating fire in set-state after fw/bw navigation

--- app/javascript/controllers/set_state_controller.js
+++ app/javascript/controllers/set_state_controller.js
@@ -13,6 +13,8 @@ export default class extends Controller {
       // here will be a race condition if the state storage is created by the same render
       // so state should already exists on the page
       this.fire();
+      // next line prevents repeating fire after bw/fw navigation
+      this.onConnectValue = false;
     }
   }
 
