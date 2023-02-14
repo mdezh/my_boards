f311d4143212615eab5ec3369a1aab2a7a7ffa9e
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-02-14 09:39:44 +0300

    broadcast notes count to board details

--- app/models/note.rb
+++ app/models/note.rb
@@ -7,12 +7,30 @@ class Note < ApplicationRecord
 
   before_validation :strip_content
 
-  broadcasts_to ->(note) { [note.board, :notes] }, inserts_by: :prepend
+  after_create_commit ->(note) { add_note note }
+  after_update_commit ->(note) { update_note note }
+  after_destroy_commit ->(note) { destroy_note note }
 
   private
 
   def strip_content
     # with content.strip! form field after invalid submit will stay unstripped
     self.content = content.strip
+  end
+
+  def add_note(note)
+    broadcast_prepend_later_to [note.board, :notes]
+    broadcast_update_later_to [note.board, :notes], target: nil, targets: '.bc-board-notes-count',
+                                                    html: note.board.notes.count
+  end
+
+  def update_note(note)
+    broadcast_replace_later_to [note.board, :notes]
+  end
+
+  def destroy_note(note)
+    broadcast_remove_to [note.board, :notes]
+    broadcast_update_to [note.board, :notes], target: nil, targets: '.bc-board-notes-count',
+                                              html: note.board.notes.count
   end
 end
